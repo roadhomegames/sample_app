080ecb6474e9ac8928c66e0c3a99d0458a8c2a24
David Blinn <roadhomegames@gmail.com>
2014-02-23 15:36:52 -0500

    completed chapter 4

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "Static pages" do
+	let(:title_str) { "Ruby on Rails Tutorial Sample App" }
 
   describe "Home page" do
 
@@ -11,7 +12,7 @@ describe "Static pages" do
 
     it "should have the right title" do
   		visit '/static_pages/home'
-  		expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+  		expect(page).to have_title("#{title_str} | Home")
 	end
   end
 
@@ -25,7 +26,7 @@ describe "Static pages" do
 
 	it "should have the right title" do
   		visit '/static_pages/help'
-  		expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+  		expect(page).to have_title("#{title_str} | Help")
 	end   
   end
 
@@ -38,7 +39,7 @@ describe "Static pages" do
 
 	it "should have the right title" do
   		visit '/static_pages/about'
-  		expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+  		expect(page).to have_title("#{title_str} | About")
 	end  
   end
 end
